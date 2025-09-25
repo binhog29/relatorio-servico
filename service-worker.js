f0a741492b0b164ef8b9861df7832c51414ae56a
Fabio Borges <fabioborges837@gmail.com>
2025-09-25 00:30:32 -0400

    Adiciona ícone e funcionalidade de PWA

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', function(event) {
+    // Este service worker não faz nada, apenas para permitir a instalação.
+    // O Vercel lida com o caching automaticamente.
+});
